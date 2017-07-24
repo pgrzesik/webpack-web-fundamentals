e6a013ea021b402500147ab8d0ad2c987c6bcd2e
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-24 22:37:57 +0200

    rewrite config to function syntax to be able to leverage `env` param

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require("path");
 
-module.exports = {
-  entry: "./src/",
-  output: {
-    filename: "[chunkhash].bundle.js",
-    path: path.join(__dirname, "dist")
-  } 
-}
+module.exports = (env) => {
+    return {
+      entry: "./src/",
+      output: {
+        filename: "[chunkhash].bundle.js",
+        path: path.join(__dirname, "dist")
+      } 
+    }
+};
