bd90554dceffdf8b3c63391fc1b6579160e25d28
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-25 23:09:29 +0200

    Add extract text webpack plugin

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,6 +1,19 @@
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ExtractTextWebpackPlugin.extract({
+          loader: "css-loader",
+          fallback: "style-loader"
+        })
+      }
+    ]
+  },
   plugins: [
-    
+    new ExtractTextWebpackPlugin("style.css")
   ]
 };
