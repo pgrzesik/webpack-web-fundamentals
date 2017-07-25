5c514af17875af18c4fed181758799f94a43bcb0
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-25 22:29:51 +0200

    Add html webpack plugin

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -1,5 +1,6 @@
 const commonPaths = require("./common-paths");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
   entry: "./src/",
@@ -8,7 +9,8 @@ const config = {
     path: commonPaths.outputPath
   },
   plugins: [
-    new webpack.ProgressPlugin()
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin()
   ]
 };
 
