747310e35d77bc3fe3979739b3586d1d19d9e466
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-25 22:25:53 +0200

    Add progress bar

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -1,11 +1,15 @@
 const commonPaths = require("./common-paths");
+const webpack = require("webpack");
 
 const config = {
   entry: "./src/",
   output: {
     filename: "[chunkhash].bundle.js",
     path: commonPaths.outputPath
-  } 
+  },
+  plugins: [
+    new webpack.ProgressPlugin()
+  ]
 };
 
 module.exports = config;
