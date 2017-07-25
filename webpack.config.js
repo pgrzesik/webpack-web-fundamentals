fa83ddba6ccd9c25593848e706d51a19c9ee6dd8
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-25 21:11:44 +0200

    Add option to use addons, use addon bundleanalyze

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 const commonConfig = require("./build-utils/webpack.common");
 const webpackMerge = require("webpack-merge");
 
+const addons = (addonsArg) => {
+    let addons = [].concat.apply([], [addonsArg]).filter(Boolean);
+
+    return addons.map((addonName) => require(`./build-utils/addons/webpack.${addonName}.js`));
+}
+
 module.exports = (env) => {
     const envConfig = require(`./build-utils/webpack.${env.env}`);
+    const mergedConfig = webpackMerge(commonConfig, envConfig, ...addons(env.addons));
 
-    return webpackMerge(commonConfig, envConfig);
+    return mergedConfig;
 };
+ 
